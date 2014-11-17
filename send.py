559a59cf01c23a99171458f3974433ae2504e990
nelwizn <nelwizn>
2014-11-17 19:35:06 +0000

    Different technique

---
+++ send.py
@@ -0,0 +1,23 @@
+import smtplib
+import sys
+
+# How to send
+HOST = "mail.sharklasers.com"
+PORT = 25
+FROM = "test@test.com"
+TO = "vkqwg+bnt8wdjnem88@sharklasers.com"
+FILE = sys.argv[1]
+
+# Get message
+f = open(FILE, "r")
+data = r.data()
+f.close()
+
+# Send
+conn = smtplib.SMTP(HOST, PORT)
+conn.ehlo_or_helo_if_needed()
+conn.sendmail(FROM, TO, data)
+conn.quit()
+
+exit(0)
+
